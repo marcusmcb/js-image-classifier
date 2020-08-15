b1218e764560e087c525ff9f9e9f01e6ca4c24cf
Marcus McBride <marcusmcb@gmail.com>
2020-08-15 12:46:17 -0700

    cleaned up css and js functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const image = document.querySelector('.image');
 const labelText = document.querySelector('.label-text');
 const accuracyText = document.querySelector('.accuracy-text');
 const predictBtn = document.querySelector('.predict-btn');
+const uploadBtn = document.querySelector('.upload-btn');
 
 const model = ml5.imageClassifier("MobileNet", modelLoaded)
 
@@ -18,8 +19,16 @@ function predict() {
         } else {
             const label = results[0].label.toUpperCase();
             const accuracy = (results[0].confidence * 100).toFixed(2);
+            console.log(accuracy)
             labelText.innerText = label;
             accuracyText.innerText = `Accuracy: ${accuracy}%`;
         }
     });
 }
+
+uploadBtn.addEventListener('change', function(event) {
+    if (event.target.files[0]) {
+        const objectURL = URL.createObjectURL(event.target.files[0]);
+        image.src = objectURL;
+    }
+})
